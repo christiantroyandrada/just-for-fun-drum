11ea337bfc7ff0189e831be655b9f55653508c86
Troy Andrada <christiantroyandrada@gmail.com>
2022-06-21 15:26:00 +0800

    added keydown event listener

--- index.js
+++ index.js
@@ -44,3 +44,50 @@ for (let index = 0; index < drum_length; index++) {
     });   
 }
 
+document.addEventListener('keydown', function(e) {
+    let sound = '';
+    let keyPressed = '';
+    try {
+        keyPressed = document.getElementById('btn_' + e.key)
+        keyPressed.style.color = '#fff';
+        setTimeout(() => {
+            keyPressed.style.color = '#db0464';
+        }, 100);
+    } catch (error) {
+        
+    }
+     
+    switch(e.key){
+        case 'w':
+                sound = new Audio('./sounds/tom-1.mp3');
+                sound.play();
+                break;
+            case 'a':
+                sound = new Audio('./sounds/tom-2.mp3');
+                sound.play();
+                break;
+            case 's':
+                sound = new Audio('./sounds/tom-3.mp3');
+                sound.play();
+                break;
+            case 'd':
+                sound = new Audio('./sounds/tom-4.mp3');
+                sound.play();
+                break;
+            case 'j':
+                sound = new Audio('./sounds/crash.mp3');
+                sound.play();
+                break;
+            case 'k':
+                sound = new Audio('./sounds/kick-bass.mp3');
+                sound.play();
+                break;
+            case 'l':
+                sound = new Audio('./sounds/snare.mp3');
+                sound.play();
+                break;
+            default:
+                console.log(`This key ${e.key} is not part of the controls`);
+                break;
+    }
+})
